edafd8539d6eb6ce9c77d65170115275c7acbbcc
rakesh000999 <rakeshjoshi6078@gmail.com>
2025-01-13 01:04:19 +0545

    database

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blogging',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306',
     }
 }
 
