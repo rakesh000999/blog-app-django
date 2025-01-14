57a94108feeb997e81f140ee6ee05124ead3f906
rakesh000999 <rakeshjoshi6078@gmail.com>
2025-01-14 22:03:44 +0545

    register

--- cms/blog/views/auth_view.py
+++ cms/blog/views/auth_view.py
@@ -1,8 +1,27 @@
 
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def register(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        # User.objects.create(
+        #     username = username,
+        #     email = email,
+        #     password = password
+        # )
+        
+        # with password hashing
+        user = User.objects.create(
+            username = username,
+            email = email
+        )
+        user.set_password(password)
+        user.save()
+        return redirect("login")
+    
     return render(request, 'auth/register.html')
 
 def login(request):
