aff06c1a324e00970e74bf99e65e0a26c0823d12
rakesh000999 <rakeshjoshi6078@gmail.com>
2025-01-15 22:23:41 +0545

    foreign key: delete authorization

--- cms/blog/models.py
+++ cms/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.                 
 class Blogs(models.Model):
@@ -6,4 +7,6 @@ class Blogs(models.Model):
     subtitle = models.CharField(max_length=255, blank=False, null=False)
     description = models.TextField()
     image = models.ImageField(upload_to='images', blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default = 1)
     created_at = models.DateTimeField(auto_now_add=True)
+    
